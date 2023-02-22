d8401e42e478df09ef0e021aea47fdfac068468a
unknown <jameskagunga2022@gmail.com>
2023-02-22 04:20:50 -0800

    adding navbar

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.scss";
+//import {Link} from "react-router-dom"
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  }
+
+
+  useEffect(() => {
+
+    window.addEventListener("scroll",isActive);
+
+    return () => {
+
+        window.removeEventListener("scroll",isActive);
+    }
+  })
+
+  const currentUser={
+    id:1,
+    username:"John Doe",
+    isSeller:true
+  }
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <span className="text">fiverr</span>
+
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+
+      {active && (
+        <>
+          <hr />
+
+          <div className="menu">
+            <span>test</span>
+            <span>test</span>
+            <span>test</span>
+            <span>test</span>
+            <span>test</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
