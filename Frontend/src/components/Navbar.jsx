cbff929dbe9d585a0010e7c904f918446453a3bf
unknown <jameskagunga2022@gmail.com>
2023-03-12 18:44:00 -0700

    worked on navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,27 +4,25 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
-  }
-
+  };
 
   useEffect(() => {
-
-    window.addEventListener("scroll",isActive);
+    window.addEventListener("scroll", isActive);
 
     return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  });
 
-        window.removeEventListener("scroll",isActive);
-    }
-  })
-
-  const currentUser={
-    id:1,
-    username:"John Doe",
-    isSeller:true
-  }
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true,
+  };
 
   return (
     <div className={active ? "navbar active" : "navbar"}>
@@ -39,8 +37,31 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166__340.jpg"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
 
